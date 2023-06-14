5c159953ecbc658fd5f8e94f11ab43d53a28cc61
Adrian G <92943726+Hifilo@users.noreply.github.com>
2023-06-14 10:30:37 -0400

    refactored code to use regular expression

--- src/extension.ts
+++ src/extension.ts
@@ -22,29 +22,8 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 function removeCommentsFromCSSBlocks(cssText: string): string {
-    let isInSelector = false;
-    let output = '';
-
-    for (let i = 0; i < cssText.length; i++) {
-        if (cssText[i] === '{') {
-            isInSelector = true;
-        } else if (cssText[i] === '}') {
-            isInSelector = false;
-        }
-
-        if (!isInSelector || cssText[i] !== '/') {
-            output += cssText[i];
-        } else if (cssText[i + 1] === '*') {
-            // Skip characters until the closing '*/' is found
-            while (cssText[i] !== '*' || cssText[i + 1] !== '/') {
-                i++;
-            }
-
-            i++; // Skip the '/'
-        }
-    }
-
-    return output;
+  return cssText.replace(/\{[^{}]*\/\*[\s\S]*?\*\/[^{}]*\}/g, (match) => {
+    return match.replace(/\/\*[\s\S]*?\*\//g, '');
+  });
 }
-
 export function deactivate() {}
